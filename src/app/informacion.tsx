fab2746e6e0208ecc5ba95353a03131ddc94164b
Gustavo Blanco <gustav@MacBook-Air-de-Gustavo.local>
2025-04-21 11:50:19 +0200

    Actualización del dashboard y redirección funcional

---
+++ src/app/informacion.tsx
@@ -0,0 +1,9 @@
+// src/app/informacion.tsx
+export default function InformacionPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl">Información sobre los cursos</h1>
+        <p>Aquí es donde pondremos detalles sobre los cursos, precios, fechas, etc.</p>
+      </div>
+    );
+  }
